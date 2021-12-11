885dbcfd64b4a572dab41452f3dbb40d45f416c4
João Antonio Soares dos Passos <jdospassos8@gmail.com>
2021-12-11 19:46:39 -0300

    filter da tabela

--- millennium-falcon/src/pages/filmes.vue
+++ millennium-falcon/src/pages/filmes.vue
@@ -7,8 +7,8 @@
     </footer>
     <table>
       <tr>
-        <th>Tilte</th>
-        <th>Episode_id</th>
+        <th v-on:click="sortTitle('title')">Tilte</th>
+        <th v-on:click="sortId('episode_id')">Episode_id</th>
         <th>Created</th>
       </tr>
       <tr id="card" v-for="filme in filmes" v-bind:key="filme.id">
@@ -27,9 +27,27 @@ export default {
   data() {
     return {
       filmes: [],
+      sortBy: "asc",
     }
   },
-  methods: {},
+  methods: {
+    sortTitle: function (dado) {
+      if (this.sortBy == "asc") {
+        this.filmes.sort(function (a, b) {
+          return a[dado].localeCompare(b[dado])
+        })
+        this.sortBy = "desc"
+      } else {
+        this.filmes.sort(function (a, b) {
+          return b[dado].localeCompare(a[dado])
+        })
+        this.sortBy = "asc"
+      }
+    },
+    sortId: function (dado) {
+      this.filmes.sort(dado)
+    },
+  },
   mounted() {
     api
       .get("films")
